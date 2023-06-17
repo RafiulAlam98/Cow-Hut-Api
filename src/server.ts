07f0eae553bc7df74bcdc2d0985e6273346e1f06
M Rafiul Alam Tonmoy <mrafiul.alam7@gmail.com>
2023-06-17 09:36:44 +0600

    database connection ready

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+async function main() {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/test");
+    console.log("database connected");
+  } catch (error) {}
+}
+
+main();
