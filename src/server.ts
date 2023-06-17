cad4f9ac646057f3b8d2064c3028ecb7559fb4f1
M Rafiul Alam Tonmoy <mrafiul.alam7@gmail.com>
2023-06-17 15:48:49 +0600

    setup eslist, prettier, lint-staged, winston logger

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
+import { errorlogger, logger } from './shared/logger'
 
 async function main() {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/test");
-    console.log("database connected");
-  } catch (error) {}
+    await mongoose.connect('mongodb://127.0.0.1:27017/test')
+    logger.info('database connected')
+  } catch (error) {
+    errorlogger.error(error)
+  }
 }
 
-main();
+main()
