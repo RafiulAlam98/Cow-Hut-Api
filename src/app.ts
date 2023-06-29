35a19078303dd42f2dc9d4d7a80e2f98bc48536a
RafiulAlam <mrafiul.alam7@gmail.com>
2023-06-29 15:48:10 +0600

    globalerrorhabdler issue resolved

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
-import cors from 'cors'
 import express, { Application } from 'express'
+
+import cors from 'cors'
+import { globalErrorHandler } from './app/middlewares/globalErrorHandler'
 import routes from './app/routes/routes'
 
 export const app: Application = express()
@@ -15,6 +17,8 @@ app.use('/api/v1/', routes)
 //   //   throw new ApiError(400, 'Error is beautiful', '')
 //   res.send('server running')
 // })
+
+app.use(globalErrorHandler)
 
 // app.use((req: Request, res: Response, next: NextFunction) => {
 //   res.status(httpStatus.NOT_FOUND).json({
--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,21 +1,30 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-console */
+
+import { ErrorRequestHandler, NextFunction, Request, Response } from 'express'
+
 import ApiError from '../errors/ApiError'
-import { ErrorRequestHandler } from 'express'
 import { IGenericErrorMessage } from '../../interfaces/error/error'
 import { ZodError } from 'zod'
 import config from '../../config'
+import { errorlogger } from '../../shared/logger'
 import { handleCastError } from '../errors/handleCastErrors'
 import { handleValidationError } from '../errors/handleValidationError'
 import { handleZodError } from '../errors/handleZodError'
 
-export const globalErrorHandler: ErrorRequestHandler = (err, req, res) => {
+export const globalErrorHandler: ErrorRequestHandler = (
+  err,
+  req:Request,
+  res:Response,
+  next:NextFunction
+) => {
   let statusCode = 500
   let message = 'Something went wrong !'
   let errorMessages: IGenericErrorMessage[] = []
 
-  // eslint-disable-next-line no-unused-expressions
   config.env === 'development'
     ? console.log('globalhandler12345', err)
-    : console.log('globalHandler', err)
+    : errorlogger.error('globalHandler', err)
 
   if (err?.name === 'ValidationError') {
     const simplifiedError = handleValidationError(err)
